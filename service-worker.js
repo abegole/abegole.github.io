fe6a94cfded527c22e7169058456430bbac9b639
Aiden BeGole <abegole@g.ucla.edu>
2025-09-07 14:18:04 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31358d9d225de1281171e0e7e5a3601a.js"
+  "/precache-manifest.fff2021e5a9b03b13e2860de79fb3b09.js"
 );
 
 self.addEventListener('message', (event) => {
